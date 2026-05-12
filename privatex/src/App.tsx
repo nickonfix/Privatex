ff8a80e242d0c639b38bf36f1c0b696990656567
nickonfix <rjnikhil45@gmail.com>
2026-05-12 08:43:41 +0530

    external gitignore out

--- privatex/src/App.tsx
+++ privatex/src/App.tsx
@@ -1,4 +1,22 @@
-function App() {
-  return 'Hello'
+import React from 'react'
+
+const App = () => {
+  return (
+    <> //fragments
+
+      <div id='dad'>
+        <h1 id='lastName'>Singh</h1>
+        <h2 id='firstName'> X1</h2>
+      </div>
+
+      <div id='Mother'>
+        <h1 id='lastName'>Singh</h1>
+        <h2 id='Firstname'> X2</h2>
+      </div>
+
+    </>
+
+  )
 }
+
 export default App
