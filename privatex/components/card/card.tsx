5a94b66a2cad0b7c629709d0e0c7df52bf259e29
nickonfix <rjnikhil45@gmail.com>
2026-05-13 23:04:11 +0530

    Image+Card

--- privatex/components/card/card.tsx
+++ privatex/components/card/card.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import "./card.css"
 const card = () => {
     return (
         <div>
-            <p>"Hello from card" </p>
+            <img className="card" src="https://i.pinimg.com/736x/87/e3/6e/87e36e462be2699104fd0cd761081095.jpg" alt="" />
+            <p>Holy fucking shit .</p>
+            <h2>Nikhil Singh</h2>
         </div>
     )
 }
