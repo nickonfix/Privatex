ff7345e5c87eb7b0bfdc71fe22e96567ce5dca1c
nickonfix <rjnikhil45@gmail.com>
2026-05-13 22:45:55 +0530

    mainfile

--- privatex/src/App.tsx
+++ privatex/src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <> //fragments
-
+    <>
       <div id='dad'>
         <h1 id='lastName'>Singh</h1>
         <h2 id='firstName'> X1</h2>
       </div>
-
       <div id='Mother'>
         <h1 id='lastName'>Singh</h1>
         <h2 id='Firstname'> X2</h2>
